056fb8f9ebccc2e9286b4fc50e042f4f0297b491
Pc Patrick Casa <patrickalvares@outlook.com>
2023-01-16 20:54:47 -0300

    Organization

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                children: [
+                  Text('Meus Cartões'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
